936358b5ab9c14316bd6fd733e689f07ae557eb7
Jian Sun <jian.sun@intel.com>
2011-10-25 06:18:11 +0200

    SMS02305575 Remove application binaries from device/imc/xmm2231gl1_0 for PDT purpose
    Remove application binaries , multimedia files and unused key layout files.
    
    Change-Id: Ic9162b6f3d99a0c922912549d3fc9da7d3ddab8f
    UTP-Id: SMS02305575

--- xmm2231gl1_0/AndroidBoard.mk
+++ xmm2231gl1_0/AndroidBoard.mk
@@ -11,33 +11,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := vkp.kcm
 include $(BUILD_KEY_CHAR_MAP)
 
-#
-# keyboard layouts
-#
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/logitech.kl:system/usr/keylayout/logitech.kl
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := logitech.kcm
-include $(BUILD_KEY_CHAR_MAP)
-
-#
-# keyboard layouts
-#
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/csrbluedev.kl:system/usr/keylayout/csrbluedev.kl
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := csrbluedev.kcm
-include $(BUILD_KEY_CHAR_MAP)
-
-#
-# keyboard layouts
-#
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/microsoft.kl:system/usr/keylayout/microsoft.kl
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := microsoft.kcm
-include $(BUILD_KEY_CHAR_MAP)
-
 # board specific init.rc, asound.conf, setHandset.sh and apns-conf.xml
 #
 ifeq ($(SYSTEM_PARTITION_PERM),RW)
@@ -65,18 +38,3 @@ PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml                         \
 	frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml           \
 	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/LauncherPro-0.8.5.apk:system/app/LauncherPro-0.8.5.apk
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/AndExplorer.apk:system/app/AndExplorer.apk
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/busybox:system/bin/busybox
-
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/video/sports_mpg4_qvga_15fps_160kbps_amr_8kHz_hinted.3gp:system/media/video/sports_mpg4_qvga_15fps_160kbps_amr_8kHz_hinted.3gp
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/audio/BrookBentonItsJustAMatterOfTime.wav:system/media/audio/BrookBentonItsJustAMatterOfTime.wav
-
