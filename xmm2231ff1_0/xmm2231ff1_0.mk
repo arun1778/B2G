9156567278c7f50ab2d3b896c93f7c69c8c53cb4
SweeYee Fonn <swee.yee.fonn@intel.com>
2011-10-28 04:27:15 +0200

    SMS02309995 Android device adaptation for XMM2231 form factor
    Device configuration change for XMM2231 form factor. Should include the following:
    media profile
    display profile
    device profile
    
    Changes should be in the /device/xmm2231_ff only.
    
    UTP-Id: SMS02309995

---
+++ xmm2231ff1_0/xmm2231ff1_0.mk
@@ -0,0 +1,47 @@
+PRODUCT_PACKAGES := \
+    AccountAndSyncSettings \
+    ApiDemos \
+    AvrcpPlayer \
+    csrApps \
+    HIDApp \
+    PrintFile \
+    PushFile \
+    RFTest \
+    SynergyFM \
+    CarHome \
+    DeskClock \
+    AlarmProvider \
+    Bluetooth \
+    Calculator \
+    Calendar \
+    Camera \
+    CertInstaller \
+    DrmProvider \
+    Email \
+    Gallery \
+    LatinIME \
+    Launcher2 \
+    Mms \
+    Music \
+    Provision \
+    Protips \
+    QuickSearchBox \
+    Settings \
+    Sync \
+    Updater \
+    CalendarProvider \
+    SyncProvider
+
+$(call inherit-product, build/target/product/generic.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-record=true
+
+PRODUCT_NAME   := xmm2231ff1_0
+PRODUCT_DEVICE := xmm2231ff1_0
+PRODUCT_BRAND  := imc
+
+include frameworks/base/data/sounds/OriginalAudio.mk
+include device/common/gps/gps_as_supl.mk
+
+
