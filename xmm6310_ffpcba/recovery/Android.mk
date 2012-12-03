9341b40da39669334253bb0948530c2c37f36d17
Qiang Rao <qiang.rao@intel.com>
2012-12-03 18:00:26 +0800

    Need new andoid device folder for XMM6310 FFPCBA build
    initial add device folder for ffpcba
    
    Backup after modify the device folder for FFPCBA
    
    virtual key map for ffpcba
    
    Change boot animation.
    
    UTP-Id: SMS03150745
    Change-Id: I0b79f4dc628e449d60e52fc8f5cadee4188272f5

---
+++ xmm6310_ffpcba/recovery/Android.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2011-2012 Intel Mobile Communications GmbH
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Nov 02 2012: Modify for xmm6310_svb
+# Dec 04 2012: Modify for XMM6310 FFPCBA build
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_SRC_FILES := recovery_ui.c
+
+# should match TARGET_RECOVERY_UI_LIB should be set in BoardConfig.mk
+LOCAL_MODULE := librecovery_ui_xmm6310_ffpcba
+
+include $(BUILD_STATIC_LIBRARY)
+
---
+++ xmm6310_ffpcba/xmm6310_ffpcba.mk
@@ -0,0 +1,116 @@
+# Copyright (C) 2011 - 2012 Intel Mobile Communications GmbH
+# Notes:
+# May 30 2012: Bootanimation support
+# Jun 07 2012: Add partial GMS package into FFRD build
+# Jun 25 2012: [XMM2231_V2:CTS]: Implement MK file modifications in order to pass PackageSignatureTest of CtsSecurityTestCases.
+# Nov 02 2012: Modify for xmm6310_svb 
+# Dec 04 2012: Modify for XMM6310 FFPCBA build
+
+BUILD_PEKALL_APP := true
+#BUILD_TARGET
+#Option: openmarket
+#Option: china
+#Option: cmcc
+BUILD_TARGET := openmarket
+TARGET_BOARD_PLATFORM := xmm6310
+ifeq ($(BUILD_TARGET), cmcc)
+PRODUCT_PACKAGE_OVERLAYS += device/pekall/cmcc/overlay
+endif
+
+#
+# Path for the Intel keys and certificates
+#
+RELEASE_KEY_PATH := device/imc/common/security
+
+# Call overlays before running other builds
+PRODUCT_PACKAGE_OVERLAYS += device/imc/xmm6310_ffpcba/overlay
+
+PRODUCT_PACKAGES := \
+    AccountAndSyncSettings \
+	AlarmClock \
+    ApiDemos \
+    AvrcpPlayer \
+    csrApps \
+    HIDApp \
+    PrintFile \
+    PushFile \
+    DeskClock \
+    AlarmProvider \
+    Bluetooth \
+    Calculator \
+    Calendar \
+    Camera \
+    CertInstaller \
+    DrmProvider \
+    Email \
+    Gallery \
+    LatinIME \
+    PinyinIME \
+    Launcher2 \
+    Mms \
+    Music \
+    Provision \
+    Protips \
+    QuickSearchBox \
+    Settings \
+    Sync \
+    Updater \
+    CalendarProvider \
+    SyncProvider \
+    Stk \
+    ClockSet \
+    copybit.$(TARGET_BOARD_PLATFORM) \
+    overlay.$(TARGET_BOARD_PLATFORM) \
+    lights.$(TARGET_BOARD_PLATFORM) \
+    gralloc.$(TARGET_BOARD_PLATFORM) \
+    libstagefrighthw \
+    libcamera \
+    sensors.$(TARGET_BOARD_PLATFORM) \
+    alsa.default \
+    chargeonly \
+    gps.$(TARGET_BOARD_PLATFORM)
+
+ifeq ($(DELIVERY),'YES')
+PRODUCT_PACKAGES += libstagefright
+endif
+
+
+$(call inherit-product, build/target/product/generic.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-record=true \
+    ro.xmm2231-ota=true \
+    ro.config.ringtone=SitarVsSitar.ogg \
+    persist.sys.language=en \
+    persist.sys.country=US \
+    windowsmgr.max_events_per_sec=240 
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number (currently 1.0)
+PRODUCT_PROPERTY_OVERRIDES += ro.opengles.version=65536
+
+PRODUCT_NAME   := xmm6310_ffpcba
+PRODUCT_DEVICE := xmm6310_ffpcba
+PRODUCT_BRAND  := imc
+PRODUCT_LOCALES :=ldpi zh_CN en_US
+
+include frameworks/base/data/sounds/OriginalAudio.mk
+include device/common/gps/gps_as_supl.mk
+
+#include English TTS lang for CTS
+include external/svox/pico/lang/PicoLangDefaultInSystem.mk
+
+PRODUCT_COPY_FILES += \
+	device/imc/xmm6310_ffpcba/quickbootanim.zip:system/media/quickbootanim.zip \
+	frameworks/base/data/sounds/effects/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg
+ 
+BUILD_WITH_PEKALL_FMRADIO := true
+
+#Include Pekall's Makefile
+ifeq ($(BUILD_PEKALL_APP), true)
+include device/pekall/pekall.mk
+endif
+
+#include GMS package
+BUILD_GMS_ALL := false
+#include external/gms/google/products/gms.mk
